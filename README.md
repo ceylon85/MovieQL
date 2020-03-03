8a20e89ce57e30c21c91e0cb4213e4bf0000e899
ceylon85 <canleez123@gmail.com>
2020-03-04 00:13:37 +0900

    graphql

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# movieql
+# MovieQL
+
+## `GraphQL` 이 해결할 수 있는 문제
+1. `Over-fetching`  
+- 요청한 영역의 정보보다, 많은 정보를 서버에서 받는 것(쓸모없는 정보도 받음)
+
+2. `Under-fetching`  
+- REST에서 하나를 완성하려고 많은 소스를 요청
+
+# 
+### `Nodemon`
+- 파일을 수정할때마다 서버를 재시작
+
+### `Playground` -> `GraphQL-yoga` 
+- `Database` 를 `test`(localhost:4000)
+#
+
+### `Resolvers`
+- `Query`를 해결(resolve)
+- `GraphQL` 서버가 `Query`나 `Mutation`의 정의를 발견하면 `Resolver`를 찾아 해당 함수를 실행
+#
+### `Mutation` 
+- `DB` 상태가 변할 때 사용 `change of state`
+- `GraphQL` 에게 `Mutation`이나 `Query`를 요청하길 원할 때 `type Query`와 `type Mutation`에 넣어야 함
+
--- graphql/db.js
+++ graphql/db.js
@@ -1,46 +1,8 @@
-let movies = [
-    {
-        id: 0,
-        name: "Star Wars",
-        score: 8
-    }, {
-        id: 1,
-        name: "Avengers4 ",
-        score: 9
-    }, {
-        id: 2,
-        name: "Load of the Rings",
-        score: 8
-    }, {
-        id: 3,
-        name: "X-Man",
-        score: 8
-    }
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json" 
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === (id));
-    return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id !== (id));
-    if (movies.length > cleanMovies.length) {
-        movies = cleanMovies;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-};
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`).then(res => res.json())
+    .then(json => json.data.movies);
+}
